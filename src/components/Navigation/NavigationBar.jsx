036c9c37c2ae7578085c2f49cf979509b9e14ae7
digimori <hanaorikuu@gmail.com>
2023-12-14 20:38:33 +0000

    Navbar for large screens completed. Toggle added for later use with smaller screens

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -1,7 +1,37 @@
 import "./NavigationBar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../../assets/logo.png";
+import { Button } from "react-bootstrap";
 
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <Navbar bg="light" expand="lg" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" />
+          Lasles<span>VPN</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-center">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="#pricing">Help</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className="signIn">Sign In</Nav.Link>
+            <Nav.Link>
+              <Button className="nav__button">Sign Up</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
---
+++ src/components/VPNButton.jsx
@@ -0,0 +1,9 @@
+import { Button } from "react-bootstrap";
+
+const VPNButton = (props) => {
+  return <Button style={{
+    
+  }}>{props}</Button>;
+};
+
+export default VPNButton;
