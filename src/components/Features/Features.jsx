769cd7147130927223bdd494361db4bddceef179
digimori <hanaorikuu@gmail.com>
2024-01-03 20:21:25 +0000

    Features component started. Images imported. Styling to be added.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,39 @@
 import "./Features.css";
+import { Container, Row, Col } from "react-bootstrap";
+import illust2 from "../../assets/Illustration2.png";
+import { FaCheckCircle } from "react-icons/fa";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <Container className="features__container">
+      <Row className="features__container-content">
+        <Col className="col-12 col-md-6">
+          <img src={illust2} alt="features illustration" />
+        </Col>
+        <Col className="features__container-content_text col-12 col-md-6">
+          <h2>We Provide Many Features You Can Use</h2>
+          <p>
+            You can explore the features that we provide with fun and have their
+            own functions each feature.
+          </p>
+          <ul className="features__list">
+            <li className="features__list-item">
+              <FaCheckCircle /> Powerful online protection.
+            </li>
+            <li className="features__list-item">
+              <FaCheckCircle /> Internet without borders.
+            </li>
+            <li className="features__list-item">
+              <FaCheckCircle /> Supercharged VPN
+            </li>
+            <li className="features__list-item">
+              <FaCheckCircle /> No specific time limits.
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Features;
