be994fab2ee2fc6b6fb759c1ca43613bef0f4941
digimori <hanaorikuu@gmail.com>
2024-01-04 20:16:48 +0000

    Margin and padding added to features

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -7,26 +7,26 @@ const Features = () => {
   return (
     <Container className="features__container">
       <Row className="features__container-content">
-        <Col className="col-12 col-md-6">
+        <Col className="col-12 col-md-6 col-lg-7">
           <img src={illust2} alt="features illustration" />
         </Col>
-        <Col className="features__container-content_text col-12 col-md-6">
-          <h2>We Provide Many Features You Can Use</h2>
-          <p>
+        <Col className="features__container-content_text col-12 col-md-6 col-lg-5">
+          <h2 className="h2__rubik">We Provide Many Features You Can Use</h2>
+          <p className="p__rubik">
             You can explore the features that we provide with fun and have their
             own functions each feature.
           </p>
           <ul className="features__list">
-            <li className="features__list-item">
+            <li className="features__list-item li__text">
               <FaCheckCircle /> Powerful online protection.
             </li>
-            <li className="features__list-item">
+            <li className="features__list-item li__text">
               <FaCheckCircle /> Internet without borders.
             </li>
-            <li className="features__list-item">
+            <li className="features__list-item li__text">
               <FaCheckCircle /> Supercharged VPN
             </li>
-            <li className="features__list-item">
+            <li className="features__list-item li__text">
               <FaCheckCircle /> No specific time limits.
             </li>
           </ul>
